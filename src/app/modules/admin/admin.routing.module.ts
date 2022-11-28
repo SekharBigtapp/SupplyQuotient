bba9ff6886ef125d74d8578f9f52c06d3dd2ed30
SekharBigtapp <chandrasekhar.g@bigtappanalytics.com>
2022-11-28 15:23:02 +0530

    Admin pages are added

--- src/app/modules/admin/admin.routing.module.ts
+++ src/app/modules/admin/admin.routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
+import { RolesComponent } from './roles/roles.component';
 
 const routes: Routes = [
-    { path: 'admin', component: AdminComponent },
+  { path: 'admin', component: AdminComponent },
+    // { path: 'users', component: AdminComponent },
+   { path: 'roles', component: RolesComponent },
+    // { path: 'rolePermission', component: AdminComponent },
+    // { path: 'memu', component: AdminComponent },
+    // { path: 'submenu', component: AdminComponent },
 ];
 
 @NgModule({
